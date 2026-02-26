c6b9da1b5b4e9802d4172e1cb9a9d89081f4843d
Iryna Serdiukova <irynaserdiukova1106@gmail.com>
2026-02-26 17:48:04 +0100

    add def __str__

--- records.py
+++ records.py
@@ -19,6 +19,9 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"    
+
     def add_phone(self, number: str):
         self.phones.append(Phone(number))
 
